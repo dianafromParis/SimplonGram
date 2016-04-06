e1a840e7e2008c5769715e589d0fd377d7a7506e
jeremie <pro_rish@yahoo.com>
2016-04-06 17:28:50 +0200

    modif et ajout style

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	var compteurBtnFollow = 0;
+	    $('.envoyer').click(function(){
+	        var toAdd = $('.envoyer_com.nico').val();
+	        console.log(toAdd);
+	        	         //$(".envoyer_com").val();
+	        $(this).siblings('.view').append("<div class='commentary'>" + 
+	        	"<a href='#' class='name_comm'>" + 
+	        	"@bidule" + "</a>" + " : " + toAdd + "</div>");
+    	});
+
+	   /* var jeremy = function(){
+	    	console.log($(this));
+	    }; */
+
+    	$('.abonner').click(function() {
+    		compteurBtnFollow++;
+    		console.log(compteurBtnFollow);
+    		if (compteurBtnFollow %2 == 0) {
+    			$('.abonner').css("background-color" , "green");
+   	
+    		}
+    		else if (compteurBtnFollow % 2 != 0) {
+    			$('.abonner').css("background-color" , "red");
+    		
+    		}
+    	});
+});
