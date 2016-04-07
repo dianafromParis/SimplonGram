e0fe94775f5404e7b04b95017dead0d26f14e30f
Asmaodeus <cadoz.olivier@gmail.com>
2016-04-07 14:55:38 +0200

    update css and js

--- js/file.js
+++ js/file.js
@@ -2,8 +2,9 @@ $(document).ready(function () {
   $('.hideOC').click(function () {
     $('.hideOC').hide();
     $('.appearOC').css('display', 'block');
-    $('instaFormOC').css('justify-content', 'flex-start');
+    $('.instaFormOC').css('justify-content', 'flex-start');
     $('.instaFormOC').css('background-color', 'rgb(255, 255, 255)');
+    $('.instaInputOC').focus();
     $('.instaInputOC').css('background-color', 'rgb(255, 255, 255)');
   });
 });
