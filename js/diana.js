a063fcdfe54226662149a399ffac03d1cedd7c73
Diana <unsupermail@gmail.com>
2016-04-07 19:11:06 +0200

    @la mano : réunion du code d'Olivier + Jehany

---
+++ js/diana.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+$('#clic1').click(function () {
+        $('.dt-picture').css({'background-image':'url(img/01.jpg)'});
+});
+
+$('#clic2').click(function () {
+        $('.dt-picture').css({'background-image':'url(img/02.jpg)'});
+});
+
+$('#clic3').click(function () {
+        $('.dt-picture').css({'background-image':'url(img/03.jpg)'});
+});
+
+$('#clic4').click(function () {
+        $('.dt-picture').css({'background-image':'url(img/04.jpg)'});
+});
+
+$('#clic5').click(function () {
+        $('.dt-picture').css({'background-image':'url(img/05.jpg)'});
+});
+
+$('#clic6').click(function () {
+        $('.dt-picture').css({'background-image':'url(img/06.jpg)'});
+});
+
+$('#clic7').click(function () {
+        $('.dt-picture').css({'background-image':'url(img/07.jpg)'});
+});
+
+$('#clic8').click(function () {
+        $('.dt-picture').css({'background-image':'url(img/08.jpg)'});
+});
+
+$('#clic9').click(function () {
+        $('.dt-picture').css({'background-image':'url(img/09.jpg)'});
+});
+
+$('#clic10').click(function () {
+        $('.dt-picture').css({'background-image':'url(img/10.jpg)'});
+});
+
+$('#clic11').click(function () {
+        $('.dt-picture').css({'background-image':'url(img/11.jpg)'});
+});
+
+$('#clic12').click(function () {
+        $('.dt-picture').css({'background-image':'url(img/12.jpg)'});
+});
+
+$('#clic13').click(function () {
+        $('.dt-picture').css({'background-image':'url(img/13.jpg)'});
+});
+
+$('#clic14').click(function () {
+        $('.dt-picture').css({'background-image':'url(img/14.jpg)'});
+});
+
+$('#clic15').click(function () {
+        $('.dt-picture').css({'background-image':'url(img/15.jpg)'});
+});
+
+});
